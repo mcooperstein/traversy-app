3abcb5fda52c199778bc8277f3f0dff5f61997d6
Marc Cooperstein <mcooperstein@gmail.com>
2018-02-12 13:46:48 -0800

    added custom click event

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -7,11 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  name = "World";
+  name:string;
+  age:number;
+  email:string;
+  /*address:{
+    street:string,
+    city:string,
+    state:string
+  }*/
+  address: Address;
+  hobbies:string[];
 
   constructor() { }
 
   ngOnInit() {
+    this.name = 'John Doe';
+    this.age = 30;
+    this.email = 'jdoe@gmail.com'
+    this.address = {
+      street: '50 Main St.',
+      city: 'Boston',
+      state: 'MA'
+    }
+    this.hobbies = ['Write code', 'Watch movies', 'Listen to music']
   }
 
+  onClick(){
+    document.getElementById("sayName").innerHTML = "Hello, " + this.name;
+  }
+
+}
+
+interface Address{
+  street:string,
+  city:string,
+  state:string
 }
