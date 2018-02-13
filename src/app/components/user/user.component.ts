b30aae5838be3a7b5c8130b927e01aac04ce8d26
Marc Cooperstein <mcooperstein@gmail.com>
2018-02-12 16:31:33 -0800

    added HttpModule for get request for fake JSON data

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { DataService } from '../../services/data.service';
+
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
@@ -17,8 +19,12 @@ export class UserComponent implements OnInit {
   }*/
   address: Address;
   hobbies:string[];
+  posts: Post[];
+  isEdit:boolean = false;
 
-  constructor() { }
+  constructor(private dataService: DataService) {
+
+  }
 
   ngOnInit() {
     this.name = 'John Doe';
@@ -29,7 +35,11 @@ export class UserComponent implements OnInit {
       city: 'Boston',
       state: 'MA'
     }
-    this.hobbies = ['Write code', 'Watch movies', 'Listen to music']
+    this.hobbies = ['Write code', 'Watch movies', 'Listen to music'];
+    this.dataService.getPosts().subscribe((posts) => {
+      //console.log(posts);
+      this.posts = posts;
+    })
   }
 
   onClick(){
@@ -50,6 +60,10 @@ export class UserComponent implements OnInit {
     this.hobbies.unshift(hobby);
   }
 
+  toggleEdit(){
+    this.isEdit = !this.isEdit;
+  }
+
   deleteHobby(hobby){
     for(let i =0; i<this.hobbies.length;i++){
       if(this.hobbies[i]==hobby){
@@ -64,4 +78,11 @@ interface Address{
   street:string,
   city:string,
   state:string
+}
+
+interface Post{
+  id: number,
+  title: string,
+  body: string,
+  userId: number
 }
